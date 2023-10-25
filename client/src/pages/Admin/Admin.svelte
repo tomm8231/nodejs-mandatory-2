84d3e133461e1d98e508e8b7bdc79caf2589af89
Tommyk <tomm8231@stud.kea.dk>
2023-10-25 12:22:13 +0200

    Login form added

--- client/src/pages/Admin/Admin.svelte
+++ client/src/pages/Admin/Admin.svelte
@@ -1,1 +1,6 @@
+<script>
+    import Login from "../Login/Login.svelte"
+</script>
 <h3>Admin page</h3>
+
+<Login />
---
+++ client/src/pages/Login/Login.svelte
@@ -0,0 +1,14 @@
+<form action="/login" method="post">
+    <label for="username">Brugernavn</label>
+    <input type="text" name="username" placeholder="Dit brugernavn" value="admin" required>
+
+    <br />
+
+    <label for="password">Kodeord</label>
+    <input type="password" name="password" placeholder="Dit kodeord" value="123456" required>
+
+    <br />
+
+    <button>Login</button>
+
+</form>
