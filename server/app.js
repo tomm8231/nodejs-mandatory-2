b04504dc7bbf707ff11485dc549cefaf22e87eaf
Tommyk <tomm8231@stud.kea.dk>
2023-10-25 13:57:46 +0200

    Express rate limit middleware added to app.js and router modules added

--- server/app.js
+++ server/app.js
@@ -2,7 +2,54 @@ import Express from "express"
 const app = Express()
 
 
+// ============== Middleware ==============
+
+import { rateLimit } from "express-rate-limit"
+
+//Taken from documentation page: https://www.npmjs.com/package/express-rate-limit
+
+const allRoutesRateLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+});
+
+app.use(allRoutesRateLimiter)
+
+const authRateLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+});
+
+app.use("/auth", authRateLimiter)
+
+
+
+// ============== Routers ==============
+
+import authRouter from "./routers/authRouter.js"
+app.use(authRouter)
+import adminRouter from "./routers/adminRouter.js"
+app.use(adminRouter)
+import homeRouter from "./routers/homeRouter.js"
+app.use(homeRouter)
+
+
+// ============== Wildcard route ==============
+
+app.all("*", (req, res) => {
+    res.status(404).send({ data: `Unsupported path ${req.path}`})
+})
+
+
+// ============== Listening to Port ==============
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
+    console.log("Server is running on port", PORT)
 })
