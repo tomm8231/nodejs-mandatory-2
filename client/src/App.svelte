26291590546e658dee0e3fd3783208e195b0b20d
Tommyk <tomm8231@stud.kea.dk>
2023-10-25 11:34:31 +0200

    Frontend initialised with Svelte

---
+++ client/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+import { Router, Link, Route } from "svelte-navigator"
+import Home from "./pages/Home/Home.svelte"
+import Admin from "./pages/Admin/Admin.svelte"
+
+</script>
+
+
+<Router>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/admin">Admin</Link>
+	</nav>
+
+	<div>
+		<Route path="/"><Home /></Route>
+		<Route path="/admin"><Admin /></Route>
+	</div>
+
+</Router>
