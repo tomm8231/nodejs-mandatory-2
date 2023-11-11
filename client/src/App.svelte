2a1a156fffad9b9be1ad219effcd184e3068b7a5
Tommyk <tomm8231@stud.kea.dk>
2023-11-11 21:36:15 +0100

    Signup dummy removed

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,7 +3,6 @@ import { Router, Link, Route } from "svelte-navigator"
 import Home from "./pages/Home/Home.svelte"
 import User from "./pages/User/User.svelte"
 import Login from "./pages/Login/Login.svelte"
-import Signup from "./pages/Signup/Signup.svelte"
 import Contact from "./pages/Contact/Contact.svelte"
 import Error from "./pages/Error/Error.svelte"
 import PrivateRoute from "./PrivateRoute.svelte"
@@ -46,12 +45,10 @@ async function handleLogout() {
 		<span>|</span>
 		<Link to="/contact">Kontakt</Link>
 		{#if !$user}
-		<span>|</span>
-			<Link to="/login">Login</Link>
 			<span>|</span>
-			<Link to="/signup">Opret bruger</Link>
+			<Link to="/login">Login</Link>
 		{:else}
-		<span>|</span>
+			<span>|</span>
 			<Link to="/user">Bruger</Link>
 			<span>|</span>
 			<a href="/" on:click={handleLogout}>Log ud</a>
@@ -62,7 +59,6 @@ async function handleLogout() {
 	<div>
 		<Route path="/"><Home /></Route>
 		<Route path="/login"><Login /></Route>
-		<Route path="/signup"><Signup /></Route>
 		<PrivateRoute path="/user" let:location><User /></PrivateRoute>
 		<Route path="/contact"><Contact /></Route>
 		<Route path="*"><Error /></Route>
